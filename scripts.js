ca58c8b407b2871a15b769305dfc2c8c9e31ecf5
MrVitalii <po4tavitaliya007@gmail.com>
2023-02-04 01:56:27 +0200

    First commit

--- scripts.js
+++ scripts.js
@@ -1,2 +1,20 @@
-let name = prompt("What is your name?");
-alert('Hello, ' + name + '! How are you?')
+let operator = prompt(`Enter operator +, -, /, *`);
+let num1 = prompt(`Enter operand A`);
+let num2 = prompt(`Enter operand B`);
+switch (operator) {
+    case '+':
+        alert(Number(num1) + Number(num2));
+        break;
+    case '-':
+        alert(Number(num1) - Number(num2));
+        break;
+    case '/':
+        alert(Number(num1) / Number(num2));
+        break;
+    case '*':
+        alert(Number(num1) * Number(num2));
+        break;
+    default:
+        alert('You must choose a number!');
+}
+
