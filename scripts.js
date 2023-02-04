c546e0c2ab5d15210c6f923180b584eae67d50fc
MrVitalii <po4tavitaliya007@gmail.com>
2023-02-04 04:34:46 +0200

    First commit

--- scripts.js
+++ scripts.js
@@ -1,20 +1,26 @@
 let operator = prompt(`Enter operator +, -, /, *`);
 let num1 = prompt(`Enter operand A`);
 let num2 = prompt(`Enter operand B`);
+let plus = (Number(num1) + Number(num2));
+let minus = (Number(num1) - Number(num2));
+let divide = (Number(num1) / Number(num2));
+let multiply = (Number(num1) * Number(num2));
+
 switch (operator) {
     case '+':
-        alert(Number(num1) + Number(num2));
+        alert(num1 + operator + num2 + "=" + plus);
         break;
     case '-':
-        alert(Number(num1) - Number(num2));
+        alert(num1 + operator + num2 + "=" + minus);
         break;
     case '/':
-        alert(Number(num1) / Number(num2));
+        alert(num1 + operator + num2 + "=" + divide);
         break;
     case '*':
-        alert(Number(num1) * Number(num2));
+        alert(num1 + operator + num2 + "=" + multiply);
         break;
     default:
         alert('You must choose a number!');
 }
+
 
