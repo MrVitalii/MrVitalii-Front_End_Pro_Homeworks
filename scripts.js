bd3a896f1852223989602397cfcd5437df76aacd
MrVitalii <po4tavitaliya007@gmail.com>
2023-02-04 04:47:45 +0200

    First commit

--- scripts.js
+++ scripts.js
@@ -8,16 +8,16 @@ let multiply = (Number(num1) * Number(num2));
 
 switch (operator) {
     case '+':
-        alert(num1 + operator + num2 + "=" + plus);
+        alert(num1 + " " + operator + " " + num2 + " " + "=" + " " + plus);
         break;
     case '-':
-        alert(num1 + operator + num2 + "=" + minus);
+        alert(num1 + " " + operator + " " + num2 + " " + "=" + " " + minus);
         break;
     case '/':
-        alert(num1 + operator + num2 + "=" + divide);
+        alert(num1 + " " + operator + " " + num2 + " " + "=" + " " + divide);
         break;
     case '*':
-        alert(num1 + operator + num2 + "=" + multiply);
+        alert(num1 + " " + operator + " " + num2 + " " + "=" + " " + multiply);
         break;
     default:
         alert('You must choose a number and operand!');
