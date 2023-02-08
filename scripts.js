c0badd6d299702c42a183304e5e9d64bb8d50aa0
MrVitalii <po4tavitaliya007@gmail.com>
2023-02-08 04:26:55 +0200

    Commit

--- scripts.js
+++ scripts.js
@@ -1,25 +1,34 @@
 //*Part 1*//
-//
-// let operator = prompt(`Enter operator +, -, /, *`);
-// let num1 = Number(prompt(`Enter operand A`));
-// let num2 = Number(prompt(`Enter operand B`));
-// let plus = ((num1) + (num2));
-// let minus = ((num1) - (num2));
-// let divide = ((num1) / (num2));
-// let multiply = ((num1) * (num2));
-//
-// switch (operator) {
-//     case '+': alert(num1 + " " + operator + " " + num2 + " " + "=" + " " + plus);
-//         break;
-//     case '-': alert(num1 + " " + operator + " " + num2 + " " + "=" + " " + minus);
-//         break;
-//     case '/': alert(num1 + " " + operator + " " + num2 + " " + "=" + " " + divide);
-//         break;
-//     case '*': alert(num1 + " " + operator + " " + num2 + " " + "=" + " " + multiply);
-//         break;
-//     default:
-//         alert('You must choose a number and operand!');
-// }
+let operator = prompt(`Enter operator +, -, /, *`);
+let num1 = validation(`Enter operand A`);
+let num2 = validation(`Enter operand B`);
+let result = count(operator, num1, num2);
+const arr = ['+', '-', '/', '*'];
+
+alert(`${num1} ${operator} ${num2} = ${result}`);
+
+
+function validation(numbers) {
+    if (Number.isNaN(numbers) === false)
+        return Number(prompt(`Enter operator ${numbers}`));
+    else if (arr.includes(`+, -, /, *`))
+        return Number(prompt(`Enter operator ${numbers}`))
+}
+
+function count(operator, num1, num2) {
+    switch (operator) {
+        case '+':
+            return num1 + num2;
+        case '-':
+            return num1 - num2;
+        case '/':
+            return num1 / num2;
+        case '*':
+            return num1 * num2;
+        default:
+            alert('You must choose a number and operand!');
+    }
+}
 
 //*Part 2*//
 const obj = {
@@ -37,7 +46,7 @@ const obj = {
             },
         },
     }],
-    prop3: function() {
+    prop3: function () {
         return {
             baz: 'Hello',
             bar: {
@@ -50,7 +59,7 @@ const obj = {
     },
 };
 
-console.log(obj. prop2 [3] ['property with spaces'].a.c.someProperty[0] ['prop name']) ; // I am a smart programmer
-console.log(obj. prop3(). bar. anotherBeautifulProp[1].target); // It was simple!
+console.log(obj.prop2 [3] ['property with spaces'].a.c.someProperty[0] ['prop name']); // I am a smart programmer
+console.log(obj.prop3().bar.anotherBeautifulProp[1].target); // It was simple!
 
 
