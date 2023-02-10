e5754843c2618b41d16b6355afb16cdd83beb316
MrVitalii <po4tavitaliya007@gmail.com>
2023-02-11 01:16:08 +0200

    First

--- scripts.js
+++ scripts.js
@@ -6,45 +6,47 @@ let num4 = Number(prompt(`Enter operand D`))
 let num5 = Number(prompt(`Enter operand E`));
 
 
-
-const result = calc(operator, num1, num2,num3,num4,num5);
-ViewResults(num1,operator, num2, operator,num3,operator,num4,operator,num5,result)
+const result = calc(operator, num1, num2, num3, num4, num5);
+ViewResults(num1, operator, num2, operator, num3, operator, num4, operator, num5, result)
 
 
 do {
     operator = prompt('Enter operator');
-}
-while (operator !== '+')
+} while (operator !== '+')
 
 
-function plus(num1,num2,num3, num4,num5,) { return num1 + num2 + num3 + num4 + num5;
-}
-
-function minus(num1,num2,num3,num4,num5,) { return num1 - num2- num3 - num4 - num5;
+function plus(num1, num2, num3, num4, num5,) {
+    return num1 + num2 + num3 + num4 + num5;
 }
 
-function divide(num1,num2,num3,num4,num5,) { return num1 / num2 / num3 / num4 / num5;
+function minus(num1, num2, num3, num4, num5,) {
+    return num1 - num2 - num3 - num4 - num5;
 }
 
-function multiply(num1,num2,num3,num4,num5,) { return num1 * num2 * num3 * num4 * num5;
+function divide(num1, num2, num3, num4, num5,) {
+    return num1 / num2 / num3 / num4 / num5;
+}
+
+function multiply(num1, num2, num3, num4, num5,) {
+    return num1 * num2 * num3 * num4 * num5;
 }
 
 function calc(operator) {
     switch (operator) {
         case '+':
-            return plus(num1, num2);
+            return plus(num1, num2, num3, num4, num5);
 
         case '-':
-            return minus(num1, num2);
+            return minus(num1, num2, num3, num4, num5);
 
         case '/':
-            return divide(num1, num2);
+            return divide(num1, num2, num3, num4, num5);
 
         case '*':
-            return multiply(num1, num2);
+            return multiply(num1, num2, num3, num4, num5);
     }
 }
 
-function ViewResults(operator, num1, num2, result) {
-    alert(`${operator} ${num1} ${num2} = ${result}`)
+function ViewResults(operator, num1, num2, num3, num4, num5, result) {
+    alert(`${operator} ${num1} ${num2} ${num3} ${num4} ${num5} = ${result}`)
 }
