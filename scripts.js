b4f356ac8a525192547665bdb877a698c12c6465
MrVitalii <po4tavitaliya007@gmail.com>
2023-02-21 11:19:44 +0200

    Commit

---
+++ scripts.js
@@ -0,0 +1,80 @@
+const QUESTIONS = [
+    {
+        // question: prompt('Сколько хромосом у здорового человека?'),
+        answer: '46',
+        type: 'prompt',
+    },
+    // {
+    //     question: confirm('Путин - хуйло?'),
+    //     answer: true,
+    //     type: 'confirm',
+    // },
+    {
+        // question: prompt('Сколько хромосом у Путина?'),
+        answer: '47',
+        type: 'prompt',
+    },
+    {
+        // question: prompt('Сколько тупых овец в московии (в млн)?'),
+        answer: '144',
+        type: 'prompt',
+    },
+    // {
+    //     question: confirm('Снесли ли памятник Екатерине-2 в Одессе?'),
+    //     answer: true,
+    //     type: 'confirm',
+    // },
+    // {
+    //     question: prompt('Сколько черных пакетов выделяются на одного орка?'),
+    //     answer: '1',
+    //     type: 'prompt',
+    // },
+    // {
+    //     question: prompt('На сколько вы оцениваете работу ЗСУ от 1 до 10?'),
+    //     answer: '10',
+    //     type: 'prompt',
+    // },
+    // {
+    //     question: prompt('Со скольких позиций готовилось нападение на Беларусь?'),
+    //     answer: '4',
+    //     type: 'prompt',
+    // },
+    // {
+    //     question: confirm('Нужно ли сжигать сосийский флаг?'),
+    //     answer: true,
+    //     type: 'confirm',
+    // },
+    // {
+    //     question: confirm('Поддерживаете ли вы уход иностранных компаний из московии?'),
+    //     answer: true,
+    //     type: 'confirm',
+    // },
+    // {
+    //     question: confirm('Считаете ли вы сосию своим домом?'),
+    //     answer: false,
+    //     type: 'confirm',
+    // },
+];
+
+
+// let score = 0
+//
+// do {
+//     score = score + 10;
+//
+// } while (prompt===QUESTIONS[0].answer);
+
+
+
+// alert("Your score is: " + score)
+
+
+
+
+console.log(QUESTIONS[0].answer)
+
+
+
+
+
+
