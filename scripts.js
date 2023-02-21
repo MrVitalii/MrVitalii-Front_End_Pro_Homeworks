8634fdb419b6dc6bf176c2ab14bbe19121089ff5
MrVitalii <po4tavitaliya007@gmail.com>
2023-02-21 21:28:25 +0200

    Commit

--- scripts.js
+++ scripts.js
@@ -1,77 +1,97 @@
 const QUESTIONS = [
     {
-        // question: prompt('Сколько хромосом у здорового человека?'),
+        question: 'Сколько хромосом у здорового человека?',
         answer: '46',
         type: 'prompt',
     },
-    // {
-    //     question: confirm('Путин - хуйло?'),
-    //     answer: true,
-    //     type: 'confirm',
-    // },
     {
-        // question: prompt('Сколько хромосом у Путина?'),
+        question: 'Путин - хуйло?',
+        answer: true,
+        type: 'confirm',
+    },
+    {
+        question: 'Сколько хромосом у Путина?',
         answer: '47',
         type: 'prompt',
     },
     {
-        // question: prompt('Сколько тупых овец в московии (в млн)?'),
+        question: 'Сколько тупых овец в московии (в млн)?',
         answer: '144',
         type: 'prompt',
     },
-    // {
-    //     question: confirm('Снесли ли памятник Екатерине-2 в Одессе?'),
-    //     answer: true,
-    //     type: 'confirm',
-    // },
-    // {
-    //     question: prompt('Сколько черных пакетов выделяются на одного орка?'),
-    //     answer: '1',
-    //     type: 'prompt',
-    // },
-    // {
-    //     question: prompt('На сколько вы оцениваете работу ЗСУ от 1 до 10?'),
-    //     answer: '10',
-    //     type: 'prompt',
-    // },
-    // {
-    //     question: prompt('Со скольких позиций готовилось нападение на Беларусь?'),
-    //     answer: '4',
-    //     type: 'prompt',
-    // },
-    // {
-    //     question: confirm('Нужно ли сжигать сосийский флаг?'),
-    //     answer: true,
-    //     type: 'confirm',
-    // },
-    // {
-    //     question: confirm('Поддерживаете ли вы уход иностранных компаний из московии?'),
-    //     answer: true,
-    //     type: 'confirm',
-    // },
-    // {
-    //     question: confirm('Считаете ли вы сосию своим домом?'),
-    //     answer: false,
-    //     type: 'confirm',
-    // },
-];
+    {
+        question: 'Снесли ли памятник Екатерине-2 в Одессе?',
+        answer: true,
+        type: 'confirm',
+    },
+    {
+        question: 'Сколько черных пакетов выделяются на одного орка?',
+        answer: '1',
+        type: 'prompt',
+    },
+    {
+        question: 'На сколько вы оцениваете работу ЗСУ от 1 до 10?',
+        answer: '10',
+        type: 'prompt',
+    },
+    {
+        question: 'Со скольких позиций готовилось нападение на Беларусь?',
+        answer: '4',
+        type: 'prompt',
+    },
+    {
+        question: 'Нужно ли сжигать сосийский флаг?',
+        answer: true,
+        type: 'confirm',
+    },
+    {
+        question: 'Поддерживаете ли вы уход иностранных компаний из московии?',
+        answer: true,
+        type: 'confirm',
+    },
+    {
+        question: 'Считаете ли вы сосию своим домом?',
+        answer: false,
+        type: 'confirm',
+    },
+]
+
+let totalScore = 0
+
+for (let i = 0; i < QUESTIONS.length; i++) {
+    let question = QUESTIONS[i]
+
+    if (question.type === 'prompt') {
+        let userAnswer = prompt(question.question)
+
+        if (userAnswer === question.answer) {
+            totalScore += 10
+        } else {
+            totalScore += 0
+        }
+
+    } else if (question.type === 'confirm') {
+        let userAnswer = confirm(question.question)
+
+        if (userAnswer === question.answer) {
+            totalScore += 10
+        } else {
+            totalScore += 0
+        }
+    }
+}
+
+alert(`Вы набрали ${totalScore} очков!`)
 
 
-// let score = 0
-//
-// do {
-//     score = score + 10;
-//
-// } while (prompt===QUESTIONS[0].answer);
-
-
-
-// alert("Your score is: " + score)
 
 
 
 
-console.log(QUESTIONS[0].answer)
+
+
+
+
 
 
 
