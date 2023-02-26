2cfd6e9c228e09802c48267cc2dc6262542d5c68
MrVitalii <po4tavitaliya007@gmail.com>
2023-02-26 19:25:43 +0200

    Commit

--- scripts.js
+++ scripts.js
@@ -1,49 +1,56 @@
-function createCalculator(base) {
-    // implement
-    let currentValue = base
+function Calculator(base) {
+    this.currentValue = base
 
-    return {
-        add: (base) => {
-            if (typeof base === 'number')
-                currentValue += base
-        },
-        sub: (base) => {
-            if (typeof base === 'number')
-                currentValue -= base
-        },
-        set: (base) => {
-            if (typeof base === 'number')
-                currentValue = base
-        },
-        get: () => {
-            return currentValue
-        },
-        reset: () => {
-            currentValue = base
+    this.add = function add(base) {
+        if (typeof base === 'number') {
+            return this.currentValue += base
         }
     }
+
+    this.sub = function sub(base) {
+        if (typeof base === 'number') {
+            return this.currentValue -= base
+        }
+    }
+
+    this.set = function set(base) {
+        if (typeof base === 'number') {
+            return this.currentValue = base
+        }
+    }
+
+    this.get = function get() {
+        return this.currentValue
+    }
+
+    this.reset = function get() {
+        this.currentValue = base
+    }
+
 }
 
 
-const calculator = createCalculator(100);
+const calc = new Calculator(100);
 
 
-calculator.add(10) // 110 - это текущее значение base
-calculator.add(10)
-calculator.sub(20)
+calc.add(10)
+calc.add(10)
+calc.sub(20)
 
 
-calculator.set(20)
-calculator.add(10)
-calculator.add(10)
-calculator.add('qwe') // NaN и значение 40 не менять
+calc.set(20)
+calc.add(10)
+calc.add(10)
+calc.add('qwe')
 
 
-console.log(calculator.get()) // 40
+console.log(calc.get()) // 40
 // ((base = 100) + 10 + 10 - 20 = 100 => (set new value = 20) + 10 + 10 + (string 'qwe' ignore) = 40)
 
 
-calculator.reset()
-console.log(calculator.get()) // 100
+calc.reset()
+console.log(calc.get()) // 100
 // (return base = 100)
+
+
 
