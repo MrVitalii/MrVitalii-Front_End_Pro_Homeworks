1dc3719a0ddb8c8f9903cbc9a0da058cae592e45
MrVitalii <po4tavitaliya007@gmail.com>
2023-03-04 01:45:29 +0200

    Commit

--- scripts.js
+++ scripts.js
@@ -1,27 +1,75 @@
 'use strict'
 
-function Hamburger() {
+function Hamburger(size) {
     // implement
+    this.price = size.price //price = chose size
+    this.calories = size.calories //calories = chose size
+    this.toppings = [] //empty massive for toppings
 }
 
-
+//Static properties
 Hamburger.SIZE_SMALL = {
+    price: 50,
+    calories: 20,
+}
+
+Hamburger.SIZE_MIDDLE = {
+    price: 75,
+    calories: 30,
+}
+
+Hamburger.SIZE_BIG = {
     price: 100,
-    callories: 8888,
+    calories: 40,
 }
 
+Hamburger.TOPPING_CHEESE = {
+    price: 10,
+    calories: 20,
+}
 
-Hamburger.TOPPING_MAYO = {
+Hamburger.TOPPING_SALAD = {
     price: 20,
-    callories: 42,
+    calories: 5,
 }
 
+Hamburger.TOPPING_POTATO = {
+    price: 15,
+    calories: 10,
+}
 
-const hamburger = new Hamburger(Hamburger.SIZE_SMALL);
+Hamburger.TOPPING_SEASONINGS = {
+    price: 15,
+    calories: 0,
+}
 
-hamburger.addTopping(Hamburger.TOPPING_MAYO);
-hamburger.addTopping(Hamburger.TOPPING_POTATO);
-hamburger.addTopping(Hamburger.TOPPING_POTATO);
+Hamburger.TOPPING_MAYONNAISE = {
+    price: 20,
+    calories: 5,
+}
 
-console.log('Price with sauce: ' + hamburger.getPrice());
-console.log('Callories with sauce: ' + hamburger.getCallories());
+//Methods in prototype
+Hamburger.prototype.addTopping = function (topping) {
+    this.toppings.push(topping) //pushing toppings to massive
+    this.price += topping.price
+    this.calories += topping.calories
+}
+
+Hamburger.prototype.getPrice = function () {
+    return this.price
+}
+
+Hamburger.prototype.getCalories = function () {
+    return this.calories
+}
+
+const hamburger = new Hamburger(Hamburger.SIZE_SMALL)
+
+hamburger.addTopping(Hamburger.TOPPING_CHEESE)
+hamburger.addTopping(Hamburger.TOPPING_SALAD)
+hamburger.addTopping(Hamburger.TOPPING_POTATO)
+hamburger.addTopping(Hamburger.TOPPING_SEASONINGS)
+hamburger.addTopping(Hamburger.TOPPING_MAYONNAISE)
+
+console.log('Price with sauce: ' + hamburger.getPrice() + ' tugriks')
+console.log('Calories with sauce: ' + hamburger.getCalories() + ' calories')
