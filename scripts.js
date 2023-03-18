97612ed5b90a8ff05def6a6ef8b905c10e1e84c7
MrVitalii <po4tavitaliya007@gmail.com>
2023-03-18 03:01:27 +0200

    Commit

--- scripts.js
+++ scripts.js
@@ -1,11 +1,14 @@
-
+const rootDiv = document.querySelector('#rootDiv')
 const ul = document.querySelector('#todoList')
 const input = document.querySelector('#msgInput')
 const button = document.querySelector('#msgButton')
 
 button.addEventListener('click', onButtonClick)
+ul.addEventListener('click', onDeleteButtonClick)
+rootDiv.addEventListener('click', onRootDivClick)
 
-function onButtonClick () {
+
+function onButtonClick() {
     const todo = getTodoData()
 
     if (!isTodoValid(todo)) {
@@ -17,25 +20,50 @@ function onButtonClick () {
     clear()
 }
 
-function getTodoData () {
-    return { message: input.value }
+function onDeleteButtonClick(e) {
+    if (e.target.classList.contains('deleteButton')) {
+        const li = e.target.closest('li')
+        ul.removeChild(li)
+    }
 }
 
-function isTodoValid (todo) { // is + validation target?
+function onRootDivClick(e) {
+    const li = e.target.closest('li')
+    if (li.style.backgroundColor === 'transparent') {
+        li.style.backgroundColor = 'aquamarine'
+    } else {
+        li.style.backgroundColor = 'transparent'
+    }
+
+}
+
+function getTodoData() {
+    return {message: input.value}
+}
+
+function isTodoValid(todo) {
     return todo.message !== ''
 }
 
-function showError () {
+function showError() {
     alert('Поле сообщение не должно быть пустым')
 }
 
-function renderTodo (todo) {
+function renderTodo(todo) {
     const li = document.createElement('li')
     li.textContent = todo.message
 
+    const deleteButton = document.createElement('button')
+    deleteButton.textContent = 'Delete'
+    deleteButton.classList.add('deleteButton')
+    li.appendChild(deleteButton)
     ul.append(li)
 }
 
-function clear () {
+function clear() {
     input.value = ''
 }
+
+
+
+
