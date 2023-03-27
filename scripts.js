c974a007844e2d989ef94b90aef2e300da67ae23
MrVitalii <po4tavitaliya007@gmail.com>
2023-03-27 23:33:32 +0300

    Commit

---
+++ scripts.js
@@ -0,0 +1,53 @@
+const msgInput = document.querySelector(('#msgInput'))
+const findUserBtn = document.querySelector(('#findUserBtn'))
+const container = document.querySelector(('#usersDataContainer'))
+
+findUserBtn.addEventListener('click', onFindUserBtnClick)
+msgInput.addEventListener('keyup', onInputKeyup)
+
+function onFindUserBtnClick() {
+    const message = msgInput.value
+
+    fetch(`https://api.github.com/users/${message}`)
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error('User is not exist')
+            }
+            return response.json()
+        })
+        .then((data) => {
+            return renderUser(data)
+        })
+        .catch((error) => {
+            alert(error.message)
+        })
+
+    return clear()
+}
+
+function onInputKeyup(event) {
+    if (event.key === 'Enter') {
+        onFindUserBtnClick()
+    }
+}
+
+function renderUser(data) {
+    const html = generateHtml(data)
+
+    container.innerHTML = html
+}
+
+function generateHtml(data) {
+    return `
+    <div>
+        <img src="${data.avatar_url}" alt="avatar">
+        <h3> Repositories: ${data.public_repos}</h3>
+        <h3> Followers: ${data.followers}</h3>
+        <h3> Following: ${data.following}</h3>
+    </div>
+  `
+}
+
+function clear() {
+    msgInput.value = ''
+}
