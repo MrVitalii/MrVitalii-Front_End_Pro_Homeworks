a65ee560a441b3c1db4b96944f0c12dfeae1891e
MrVitalii <po4tavitaliya007@gmail.com>
2023-04-07 02:05:39 +0300

    Final commit

--- ContactsApi.js
+++ ContactsApi.js
@@ -1,41 +1,44 @@
 class ContactsApi {
-    static API = 'https://6426d763556bad2a5b596110.mockapi.io/api/contacts'
+    static URL = 'https://6426d763556bad2a5b596110.mockapi.io/api/contacts';
+
+    static request(url = '', method = 'GET', body) {
+        return fetch(ContactsApi.URL + url, {
+            method,
+            body: body ? JSON.stringify(body) : undefined,
+            headers: {
+                'Content-type': 'application/json',
+            }
+        })
+            .then((res) => {
+                if (res.ok) {
+                    return res.json();
+                }
+
+                throw new Error('Can not execute request to server');
+            })
+    }
 
     static getList() {
-        return fetch(ContactsApi.API).then((res) => {
-            if (res.ok) {
-                return res.json()
-            }
-
-            throw new Error('Can not retrieve contact list from server.');
+        return ContactsApi.request().catch(() => {
+            throw new Error('Can not fetch contact list from server');
         })
     }
 
-    static create(todo) {
-        return fetch(ContactsApi.API, {
-            method: 'POST',
-            body: JSON.stringify(todo),
-            headers: {
-                'Content-type': 'application/json'
-            }
-        }).then((res) => {
-            if (res.ok) {
-                return res.json()
-            }
+    static create(contact) {
+        return ContactsApi.request('', 'POST', contact).catch(() => {
+            throw new Error('Can not create contact on server');
+        })
+    }
 
-            throw new Error('Can not create contact on server.');
+    static update(id, changes) {
+        return ContactsApi.request(`/${id}`, 'PUT', changes).catch(() => {
+            throw new Error('Can not update contact on server');
         })
     }
 
     static delete(id) {
-        return fetch(`https://6426d763556bad2a5b596110.mockapi.io/api/contacts/${id}`, {
-            method: 'DELETE',
-        }).then(res => {
-            if (res.ok) {
-                return res.json();
-            }
-
-            throw new Error('Can not delete contact from server.');
+        return ContactsApi.request(`/${id}`, 'DELETE').catch(() => {
+            throw new Error('Can not delete contact on server');
         })
     }
 }
--- scripts.js
+++ scripts.js
@@ -1,115 +1,180 @@
-const contactsTable = document.querySelector('#contactsTable')
-const contactsList = document.querySelector('#ContactsList')
-const nameInput = document.querySelector('#nameInput')
-const surnameInput = document.querySelector('#surnameInput')
-const phoneInput = document.querySelector('#phoneInput')
-const contactInputs = document.querySelector('#contactInputs')
-const button = document.querySelector('#actionButton')
+const CLASS_CONTACT_ITEM = 'contactItem'
+const CLASS_DELETE_BTN = 'deleteBtn'
+const CLASS_EDIT_BTN = 'editBtn'
+const contactContainer = document.querySelector('#contactsContainer')
+const form = document.querySelector('#contactForm')
+let contactList = []
 
+form.addEventListener('submit', onFormSubmit)
+form.addEventListener('keyup', onContactInputsKeyup)
+contactContainer.addEventListener('click', onContactContainerClick)
 
-button.addEventListener('click', onButtonClick)
-contactsList.addEventListener('click', onContactsList)
-contactInputs.addEventListener('keyup', onContactInputsKeyup)
+ContactsApi
+    .getList()
+    .then((list) => {
+        renderContactList(list)
+        contactList = list
+    })
+    .catch(e => showError(e))
 
-init()
+function onFormSubmit(e) {
+    e.preventDefault()
 
-function init() {
-    ContactsApi
-        .getList()
-        .then((list) => {
-            renderContactList(list)
-        })
-        .catch(err => showError(err))
-}
-
-function onButtonClick() {
-    const contact = getContact()
+    const contact = getContactData()
 
     if (!isContactValid(contact)) {
-        alert('Некорректные данные. Заполните поля Name, Surname, Phone');
+        showError(new Error('Некорректные данные. Заполните поля Name, Last name, Phone'))
         return
     }
 
-    ContactsApi
-        .create(contact)
-        .then((newContact) => {
-            renderContact(newContact)
-            clearForms()
-        })
-        .catch(err => showError(err))
-}
-
-function onContactsList(e) {
-    const tr = e.target.closest('tr')
-    const id = tr.getAttribute('id')
-
-    if (e.target.classList.contains('deleteBtn')) {
+    if (contact.id) {
         ContactsApi
-            .delete(id)
-            .then(() => {
-                deleteContact(tr)
+            .update(contact.id, contact)
+            .then((newContact) => {
+                replaceContact(contact.id, newContact)
+                clearForm()
+                contactList = contactList.map(contactItem => contactItem.id === contact.id ? newContact : contactItem)
             })
-            .catch(err => showError(err))
+            .catch(e => showError(e))
+    } else {
+        ContactsApi
+            .create(contact)
+            .then((newContact) => {
+                renderContact(newContact)
+                clearForm()
+                contactList.push(newContact)
+            })
+            .catch(e => showError(e))
     }
 }
 
 function onContactInputsKeyup(e) {
     if (e.key === 'Enter') {
-        onButtonClick()
+        onFormSubmit()
     }
 }
 
-function getContact() {
-    return {
-        name: nameInput.value.trim(),
-        surname: surnameInput.value.trim(),
-        phone: phoneInput.value.trim(),
+function onContactContainerClick(e) {
+    const target = e.target
+    const contactEl = findContactEl(target)
+
+    if (!contactEl) {
+        return
     }
+    if (isDeleteBtn(target)) {
+        deleteContactEl(contactEl)
+    } else if (isEditBtn(target)) {
+        editContactEl(contactEl)
+    }
+}
+
+function isDeleteBtn(el) {
+    return el.classList.contains(CLASS_DELETE_BTN)
+}
+
+function isEditBtn(el) {
+    return el.classList.contains(CLASS_EDIT_BTN)
+}
+
+function findContactEl(el) {
+    return el.closest('.' + CLASS_CONTACT_ITEM)
+}
+
+function getContactData() {
+    const id = form.id.value
+    const contact = findContactById(id) || {}
+
+    return {
+        ...contact,
+        name: form.name.value,
+        lastName: form.lastName.value,
+        phone: form.phone.value,
+    }
+}
+
+function deleteContactEl(el) {
+    const id = getContactElId(el)
+
+    ContactsApi
+        .delete(id)
+        .catch(e => showError(e))
+
+    el.remove()
+    contactList = contactList.filter(contactItem => contactItem.id !== id)
+}
+
+function editContactEl(el) {
+    const id = getContactElId(el)
+    const contact = findContactById(id)
+
+    fillForm(contact)
 }
 
 function isContactValid(contact) {
     return (
-        contact.name !== '' && contact.surname !== '' && contact.phone !== '' && !isNaN(contact.phone)
-    );
+        contact.name !== '' && contact.lastName !== '' && contact.phone !== '' && !isNaN(contact.phone)
+    )
+}
+
+function renderContactList(list) {
+    const html = list.map(generateContactHtml).join('')
+
+    contactContainer.innerHTML = html
+}
+
+function replaceContact(id, contact) {
+    const oldContactEl = document.querySelector(`[data-id="${id}"]`)
+    const newContactEl = generateContactHtml(contact)
+
+    oldContactEl.outerHTML = newContactEl
+}
+
+function renderContact(contact) {
+    const html = generateContactHtml(contact)
+
+    contactContainer.insertAdjacentHTML('beforeend', html)
+}
+
+function generateContactHtml(contact) {
+    const done = contact.done ? ' done' : ''
+    return `
+    <tr
+      class="contactItem"
+      data-id="${contact.id}"
+    >
+      <td class="txt-center">${contact.name}</td>
+      <td class="txt-center">${contact.lastName}</td>
+      <td class="txt-center">${contact.phone}</td>
+      <td>
+        <span>
+            <button class="editBtn">Edit</button>
+            <button class="deleteBtn">Delete</button>
+        </span>
+      </td>
+    </tr>
+  `
+}
+
+function clearForm() {
+    form.reset()
+    form.id.value = ''
+}
+
+function fillForm(contact) {
+    form.id.value = contact.id
+    form.name.value = contact.name
+    form.lastName.value = contact.lastName
+    form.phone.value = contact.phone
 }
 
 function showError(error) {
     alert(error.message)
 }
 
-function renderContactList(list) {
-    const html = list.map(generateContactHtml).join('')
-
-    contactsList.innerHTML = html
+function getContactElId(el) {
+    return el.dataset.id
 }
 
-function renderContact() {
-    const contact = getContact()
-    const html = generateContactHtml(contact)
-    const tbody = contactsTable.querySelector('tbody')
-    tbody.insertAdjacentHTML('beforeend', html)
-}
-
-function generateContactHtml(contact) {
-    return `
-      <tr id="${contact.id}">
-      <td class="txt-center">${contact.name}</td>
-      <td class="txt-center">${contact.surname}</td>
-      <td class="txt-center">${contact.phone}</td>
-      <td>
-      <button type="button" class="editBtn">Edit</button>
-      <button type="button" class="deleteBtn">Delete</button>
-      </td>
-    </tr>
-  `
-}
-
-function deleteContact(tr) {
-    contactsList.removeChild(tr)
-}
-
-function clearForms() {
-    nameInput.value = ''
-    surnameInput.value = ''
-    phoneInput.value = ''
+function findContactById(id) {
+    return contactList.find(contact => contact.id === id)
 }
